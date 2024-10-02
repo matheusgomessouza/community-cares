0ca38dce7334c9e66dc3eb29e4df1cd60ad081a4
matheusgomessouza <matheusg_souza@outlook.com>
2024-10-01 21:23:58 -0300

    refact: Creating FrequentlyAskedQuestionCardComponent

--- src/app/faq.tsx
+++ src/app/faq.tsx
@@ -2,18 +2,14 @@ import { router } from "expo-router";
 import { SafeAreaView, Text, StyleSheet, View, ScrollView } from "react-native";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
+import { FrequentlyAskedQuestionCardComponent } from "components/FrequentlyAskedQuestionCard";
+
 export default function FrequentlyAskedQuestionsScreen() {
   return (
     <SafeAreaView style={styles.container}>
-      <View
-        style={{
-          flexDirection: "row",
-          width: "100%",
-          justifyContent: "center",
-        }}
-      >
+      <View style={styles.headerContainer}>
         <Icon
-          style={{ marginLeft: 0, marginRight: "auto" }}
+          style={styles.backButton}
           name="chevron-left"
           size={24}
           color="#C76E16"
@@ -22,78 +18,15 @@ export default function FrequentlyAskedQuestionsScreen() {
           }}
         />
 
-        <View style={{ marginRight: "auto", alignItems: "center" }}>
+        <View style={styles.headerText}>
           <Text style={styles.brandName}>Community Cares</Text>
           <Text style={styles.subtitle}>Frequently Asked Questions</Text>
         </View>
       </View>
-      <ScrollView style={{ marginTop: 40, width: "100%" }} scrollEnabled>
-        <View style={styles.answerContainer}>
-          <View style={styles.icon}>
-            <Text style={styles.symbol}>?</Text>
-          </View>
-          <Text
-            style={{
-              flexWrap: "wrap",
-              fontSize: 8,
-              fontFamily: "Montserrat_400Regular",
-              width: "75%",
-              textAlign: "justify",
-              color: "#9E5811"
-            }}
-          >
-            It has survived not only five centuries, but also the leap into
-            electronic typesetting, remaining essentially unchanged. It was
-            popularised in the 1960s with the release of Letraset sheets
-            containing Lorem Ipsum passages, and more recently with desktop
-            publishing software like Aldus PageMaker including versions of Lorem
-            Ipsum.
-          </Text>
-        </View>
-        <View style={styles.answerContainer}>
-          <View style={styles.icon}>
-            <Text style={styles.symbol}>?</Text>
-          </View>
-          <Text
-            style={{
-              flexWrap: "wrap",
-              fontSize: 8,
-              fontFamily: "Montserrat_400Regular",
-              width: "75%",
-              textAlign: "justify",
-              color: "#9E5811"
-            }}
-          >
-            It has survived not only five centuries, but also the leap into
-            electronic typesetting, remaining essentially unchanged. It was
-            popularised in the 1960s with the release of Letraset sheets
-            containing Lorem Ipsum passages, and more recently with desktop
-            publishing software like Aldus PageMaker including versions of Lorem
-            Ipsum.
-          </Text>
-        </View>
-        <View style={styles.answerContainer}>
-          <View style={styles.icon}>
-            <Text style={styles.symbol}>?</Text>
-          </View>
-          <Text
-            style={{
-              flexWrap: "wrap",
-              fontSize: 8,
-              fontFamily: "Montserrat_400Regular",
-              width: "75%",
-              textAlign: "justify",
-              color: "#9E5811"
-            }}
-          >
-            It has survived not only five centuries, but also the leap into
-            electronic typesetting, remaining essentially unchanged. It was
-            popularised in the 1960s with the release of Letraset sheets
-            containing Lorem Ipsum passages, and more recently with desktop
-            publishing software like Aldus PageMaker including versions of Lorem
-            Ipsum.
-          </Text>
-        </View>
+      <ScrollView style={styles.questionScroll} scrollEnabled>
+        <FrequentlyAskedQuestionCardComponent />
+        <FrequentlyAskedQuestionCardComponent />
+        <FrequentlyAskedQuestionCardComponent />
       </ScrollView>
     </SafeAreaView>
   );
@@ -108,41 +41,22 @@ const styles = StyleSheet.create({
     paddingHorizontal: 32,
     paddingVertical: 64,
   },
+  headerContainer: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "center",
+  },
+  backButton: { marginLeft: 0, marginRight: "auto" },
+  headerText: { marginRight: "auto", alignItems: "center" },
   brandName: {
     fontFamily: "Shrikhand_400Regular",
     color: "#C76E16",
-    fontSize: 16,
+    fontSize: 20,
   },
   subtitle: {
     fontFamily: "Montserrat_300Light_Italic",
-    fontSize: 8,
+    fontSize: 12,
+    color: "#9F9B9B"
   },
-  icon: {
-    backgroundColor: "#FFF",
-    borderRadius: 100,
-    width: 60,
-    height: 60,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  symbol: {
-    color: "#C76E16",
-    fontFamily: "Shrikhand_400Regular",
-    fontSize: 24,
-  },
-  answerContainer: {
-    backgroundColor: "rgba(199, 110, 22, 0.4)",
-    width: "100%",
-    borderRadius: 20,
-    minHeight: 100,
-    maxHeight: "auto",
-    height: "auto",
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
-    gap: 8,
-    paddingHorizontal: 20,
-    paddingVertical: 16,
-    marginBottom: 40,
-  },
+  questionScroll: { marginTop: 40, width: "100%" },
 });
